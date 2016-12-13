62717f63296baab77aea2eeecb266c3fac4c1f18
gas ⁫⁫⁫ <gas >
2016-12-14 00:05:23 +0300

    LongInt constructor is done

--- LongInt.js
+++ LongInt.js
@@ -1,42 +1,83 @@
 function testLongInt() {
-    var _limits = new onCalc.Limiter();
     var i1 = new onCalc.LongInt(100);
     var i2 = new onCalc.LongInt("100500");
 }
 var onCalc;
 (function (onCalc) {
-    var Limiter = (function () {
-        function Limiter() {
-            for (var i = 32;; i++) {
-                var value = Math.pow(2, i);
-                var str_value = value.toFixed();
-                if (value != Math.floor((new Number(str_value)))) {
-                    break;
-                }
-                else {
-                    this.maxStoredNumber = value;
-                    this.digitsInNumber = str_value.length;
-                }
+    var Preprocessor = (function () {
+        function Preprocessor() {
+            /* Evaluation of precision of the number is not to lose accuracy of calculations.
+               It must be carried out the system of equations:
+             ╓ N = 10^k
+             ╢ n = N - 1
+             ╙ 3 = n*N - (n*n+n-3)
+            */
+            var d = 1;
+            var k = 0;
+            var n = d - 1;
+            while (((n * d) - (n * n + n - 3)) == 3) {
+                k++;
+                d *= 10;
+                n = d - 1;
             }
+            this.digitLength = k - 1;
+            this.digitAbs = 1;
+            for (var i = 0; i < this.digitLength; i++)
+                this.digitAbs *= 10;
+            //Prepare regexp for spliting string value to tokens 
+            this._splitter = new RegExp(".{1," + this.digitLength + "}", "g");
         }
-        return Limiter;
+        Preprocessor.prototype.tokenize = function (value) {
+            var head_size = value.length % this.digitLength;
+            var ret;
+            if (head_size && (head_size < value.length)) {
+                ret = [value.slice(0, head_size)].concat(value.slice(head_size).match(this._splitter));
+            }
+            else {
+                ret = value.match(this._splitter);
+            }
+            return ret;
+        };
+        Preprocessor.isNegative = function (value) {
+            return value.charAt(0) === '-';
+        };
+        return Preprocessor;
     }());
-    onCalc.Limiter = Limiter;
+    var _preprocessor = new Preprocessor();
+    //The BCD-like "unlimited" long integer number
     var LongInt = (function () {
         function LongInt(value) {
             this._initialize(value);
         }
         LongInt.prototype._initializeNumber = function (value) {
-            this._sign = value < 0;
+            this._negative = value < 0;
             this._data = new Array(1);
             this._data[0] = Math.abs(value);
         };
         LongInt.prototype._initializeString = function (value) {
             if (!value.length)
                 this._initializeNumber(0);
-            this._sign = value.charAt(0) !== '-';
-            /*
-                        this._data.push(Math.abs(value));*/
+            if (value.length <= _preprocessor.digitLength) {
+                this._initializeNumber(parseInt(value));
+            }
+            else {
+                var data = null;
+                this._negative = Preprocessor.isNegative(value);
+                if (this._negative)
+                    value = value.slice(1);
+                for (var _i = 0, _a = _preprocessor.tokenize(value); _i < _a.length; _i++) {
+                    var token = _a[_i];
+                    var digit = parseFloat(token);
+                    if (digit && !data) {
+                        data = new Array();
+                    }
+                    if (data) {
+                        data.push(digit);
+                    }
+                }
+                if (data)
+                    this._data = data.reverse();
+            }
         };
         LongInt.prototype._initialize = function (value) {
             if (!value)
@@ -58,8 +99,20 @@ var onCalc;
             this._initialize(value);
             return this;
         };
+        LongInt.prototype.parse = function (value) {
+            this._initialize(value);
+        };
         return LongInt;
     }());
     onCalc.LongInt = LongInt;
+    function x() {
+        var i = new LongInt("1234567890");
+        i = new LongInt("-1234567890");
+        i = new LongInt("12345678909876543210");
+        i = new LongInt("12345678909876543210");
+        i = new LongInt("-00000000005");
+        i = new LongInt("00000000000000000500");
+    }
+    x();
 })(onCalc || (onCalc = {}));
 //# sourceMappingURL=LongInt.js.map
--- LongInt.ts
+++ LongInt.ts
@@ -6,39 +6,67 @@ function testLongInt()
 
 namespace onCalc
 {
-    class Limiter
+    class Preprocessor
     {
+        private _splitter: RegExp;
         constructor()
         {
-            for(let i: number = 32;; i++)
+/* Evaluation of precision of the number is not to lose accuracy of calculations.
+   It must be carried out the system of equations:
+ ╓ N = 10^k
+ ╢ n = N - 1
+ ╙ 3 = n*N - (n*n+n-3)
+*/
+            let d: number = 1;
+            let k: number = 0;
+            let n: number = d - 1;
+            while(((n*d) - (n*n + n - 3)) == 3)
             {
-                let value = Math.pow(2, i);
-                let str_value = value.toFixed();
-                if(str_value.toLowerCase().indexOf('e') !== -1)
-                {
-                    break;
-                }
-                else
-                {
-                    this.maxStoredNumber = value;
-                    this.digitsInNumber = str_value.length;
-                }
+                k++;
+                d *= 10;
+                n = d - 1;
             }
+            this.digitLength = k - 1;
+            this.digitAbs = 1;
+            for(let i: number = 0; i < this.digitLength; i++)
+                this.digitAbs *= 10;
+            //Prepare regexp for spliting string value to tokens 
+            this._splitter = new RegExp(".{1," + this.digitLength + "}", "g");
         }
-        public maxStoredNumber: number;
-        public digitsInNumber: number;
+
+        public digitAbs: number; //Absolute value of LongInt digit.
+        public digitLength: number; //String length, for representation one digit (use in parse method)
+        public tokenize(value: string): Array<string>
+        {
+            let head_size = value.length % this.digitLength;
+            let ret: Array<string>;
+            if(head_size && (head_size < value.length))
+            {
+                ret = [value.slice(0, head_size)].concat(value.slice(head_size).match(this._splitter));
+            }
+            else
+            {
+                ret = value.match(this._splitter);
+            }
+            return ret;
+        }
+        public static isNegative(value: string) : boolean
+        {
+            return value.charAt(0) === '-';
+        }
     }
-    
-    const _limits: Limiter = new Limiter();
+
+    const _preprocessor = new Preprocessor();
     type ValueType = string | number;
+    //The BCD-like "unlimited" long integer number
     export class LongInt
     {
-        private _sign: boolean;
+        private _negative: boolean;
         private _data: Array<number>;
 
         private _initializeNumber(value: number)
         {
-            this._sign = value < 0;
+            this._negative = value < 0;
             this._data = new Array<number>(1);
             this._data[0] = Math.abs(value);
         }
@@ -46,9 +74,31 @@ namespace onCalc
         {
             if(!value.length)
                 this._initializeNumber(0);
-            this._sign = value.charAt(0) !== '-';
-/*
-            this._data.push(Math.abs(value));*/
+            if(value.length <= _preprocessor.digitLength)
+            {
+                this._initializeNumber(parseInt(value));
+            }
+            else
+            {
+                let data: Array<number> = null;
+                this._negative = Preprocessor.isNegative(value);
+                if (this._negative)
+                    value = value.slice(1);
+                for (let token of _preprocessor.tokenize(value))
+                {
+                    let digit = parseFloat(token);
+                    if (digit && !data)
+                    {
+                        data = new Array<number>();
+                    }
+                    if (data)
+                    {
+                        data.push(digit);
+                    }   
+                }
+                if (data)
+                    this._data=data.reverse();
+            }
         }
 
         private _initialize(value?: ValueType)
@@ -81,5 +131,21 @@ namespace onCalc
             this._initialize(value);
             return this;
         }
+
+        public parse(value: string) : void
+        {
+            this._initialize(value);
+        }
     }
+    function x()
+    {
+        let i = new LongInt("1234567890");
+        i = new LongInt("-1234567890");
+        i = new LongInt("12345678909876543210");
+        i = new LongInt("-12345678909876543210");
+        i = new LongInt("-00000000005");
+        i = new LongInt("00000000000000000500");
+        
+    }
+    x();
 }
