e11f0ffa05aaa1469c434c2cdc1d7d1070378250
gas ⁫⁫⁫ <gas >
2016-12-17 13:35:52 +0300

    Extended unit tests

--- UnitTests.js
+++ UnitTests.js
@@ -1,4 +1,9 @@
 /// <reference path="LongInt.ts"/>
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var Tests;
 (function (Tests) {
     function EXPECT_EQ(expected, actual) {
@@ -17,23 +22,32 @@ var Tests;
             throw new EvalError("Expected: " + expected.toString() + ". Actual: " + actual.toString());
         }
     }
-    var AnySignUnitTests = (function () {
-        function AnySignUnitTests(negative) {
+    var UnitTestsBase = (function () {
+        function UnitTestsBase(negative) {
+            var _this = this;
+            this.negative = function () { return _this._negative; };
             this._negative = negative;
         }
-        AnySignUnitTests.prototype.longInt = function (value) {
+        UnitTestsBase.prototype.longInt = function (value) {
             switch (typeof (value)) {
                 case "string":
-                    return new onCalc.LongInt(this._negative ? "-" + value : value);
+                    return new onCalc.LongInt(this.str(value));
                 case "number":
-                    return new onCalc.LongInt(this._negative ? -value : value);
+                    return new onCalc.LongInt(this.negative() ? -value : value);
                 default:
                     throw new Error("Initialize value type not supported");
             }
         };
-        AnySignUnitTests.prototype.str = function (value) {
-            return ((this._negative && value !== "0") ? ("-" + value) : value);
+        UnitTestsBase.prototype.str = function (value) {
+            return ((this.negative() && value !== "0") ? ("-" + value) : value);
         };
+        return UnitTestsBase;
+    }());
+    var AnySignUnitTests = (function (_super) {
+        __extends(AnySignUnitTests, _super);
+        function AnySignUnitTests(negative) {
+            return _super.call(this, negative) || this;
+        }
         AnySignUnitTests.prototype.numberToString = function () {
             var i = this.longInt(0);
             EXPECT_EQ(this.str("0"), i.toString());
@@ -42,15 +56,15 @@ var Tests;
             i = this.longInt(100500);
             EXPECT_EQ(this.str("100500"), i.toString());
             EXPECT_EQ(1, i.size());
-            EXPECT_EQ(this._negative, i.negative());
+            EXPECT_EQ(this.negative(), i.negative());
             i = this.longInt(10000020000000);
             EXPECT_EQ(this.str("10000020000000"), i.toString());
             EXPECT_EQ(2, i.size());
-            EXPECT_EQ(this._negative, i.negative());
+            EXPECT_EQ(this.negative(), i.negative());
             i = this.longInt(9876543210123456);
             EXPECT_EQ(this.str("9876543210123456"), i.toString());
             EXPECT_EQ(3, i.size());
-            EXPECT_EQ(this._negative, i.negative());
+            EXPECT_EQ(this.negative(), i.negative());
         };
         AnySignUnitTests.prototype.equalNumberAndStringConstruction = function () {
             var x = this.longInt(0);
@@ -75,21 +89,93 @@ var Tests;
             y = this.longInt("31415926535897932");
             EXPECT_EQ(x, y);
         };
+        AnySignUnitTests.prototype.equalOperatorTrue = function () {
+            var eq = function (x, y) {
+                EXPECT_EQ(true, x.equal(x));
+                EXPECT_EQ(true, y.equal(y));
+                EXPECT_EQ(true, y.equal(x));
+                EXPECT_EQ(true, x.equal(y));
+            };
+            eq(this.longInt(0), this.longInt(0));
+            eq(this.longInt(10), this.longInt(10));
+            eq(this.longInt(100500), this.longInt(100500));
+            eq(this.longInt(99980000997766), this.longInt(99980000997766));
+            eq(this.longInt("9999999000000009999997733"), this.longInt("9999999000000009999997733"));
+            eq(this.longInt("9876543219876543210987654321123456789123456789123456789"), this.longInt("9876543219876543210987654321123456789123456789123456789"));
+        };
+        AnySignUnitTests.prototype.equalOperatorFalseForSameSing = function () {
+            var eq = function (x, y) {
+                EXPECT_EQ(false, y.equal(x));
+                EXPECT_EQ(false, x.equal(y));
+            };
+            eq(this.longInt(1), this.longInt(0));
+            eq(this.longInt(10), this.longInt(990010));
+            eq(this.longInt(100501), this.longInt(100500));
+            eq(this.longInt(99980000997766), this.longInt(99980000997767));
+            eq(this.longInt("9999999000000009999997732"), this.longInt("9999999000000009999997733"));
+            eq(this.longInt("9876543219876543210987654321123456789123456789123456789"), this.longInt(98));
+            eq(this.longInt("98765432198765432109876543211234567891234567891234567890000000001"), this.longInt("98765432198765432109876543211234567891234567891234567890000000000"));
+        };
         AnySignUnitTests.prototype.add = function () {
             //99009900990099009900 + 998877665544332211 = 100008778655643342111
             var result = new onCalc.LongInt("99009900990099009900").add(new onCalc.LongInt("998877665544332211"));
             EXPECT_EQ("100008778655643342111", result.toString());
         };
         return AnySignUnitTests;
-    }());
+    }(UnitTestsBase));
+    var signRelatedUnitTests = (function (_super) {
+        __extends(signRelatedUnitTests, _super);
+        function signRelatedUnitTests() {
+            return _super.call(this, false) || this;
+        }
+        signRelatedUnitTests.prototype.lessPositiveAndPositive = function () {
+            var x = this.longInt(0);
+            var y = this.longInt(10);
+            EXPECT_EQ(true, x.less(y));
+            EXPECT_EQ(false, y.less(x));
+        };
+        signRelatedUnitTests.prototype.equalOperatorFalse = function () {
+            var _this = this;
+            var eq = function (x, y) {
+                var lx = _this.longInt(x);
+                var ly = _this.longInt(y);
+                EXPECT_EQ(false, ly.equal(lx));
+                EXPECT_EQ(false, lx.equal(ly));
+                lx = _this.longInt("-" + x);
+                EXPECT_EQ(false, ly.equal(lx));
+                EXPECT_EQ(false, lx.equal(ly));
+                ly = _this.longInt("-" + y);
+                EXPECT_EQ(false, ly.equal(lx));
+                EXPECT_EQ(false, lx.equal(ly));
+                lx = _this.longInt(x);
+                ly = _this.longInt("-" + y);
+                EXPECT_EQ(false, ly.equal(lx));
+                EXPECT_EQ(false, lx.equal(ly));
+            };
+            eq("1", "2");
+            eq("100500", "100499");
+            eq("10000000000000500000000000000", "100500");
+            eq("9099990000500000000000000", "1059999999999988760");
+            eq("9876543219876543210987654321123456789123456789123456789", "98");
+            eq("98765432198765432109876543211234567891234567891234567890000000001", "98765432198765432109876543211234567891234567891234567890000000000");
+        };
+        return signRelatedUnitTests;
+    }(UnitTestsBase));
     function RunAllTests() {
         try {
             var positive = new AnySignUnitTests(false);
             positive.numberToString();
             positive.equalNumberAndStringConstruction();
+            positive.equalOperatorTrue();
+            positive.equalOperatorFalseForSameSing();
             var negative = new AnySignUnitTests(true);
             negative.numberToString();
             negative.equalNumberAndStringConstruction();
+            negative.equalOperatorTrue();
+            negative.equalOperatorFalseForSameSing();
+            var sr = new signRelatedUnitTests();
+            sr.lessPositiveAndPositive();
+            sr.equalOperatorFalse();
             alert("ALL TESTS PASSED");
         }
         catch (ex) {
--- UnitTests.ts
+++ UnitTests.ts
@@ -21,8 +21,8 @@ module Tests
             throw new EvalError("Expected: " + expected.toString() + ". Actual: " + actual.toString());
         }
     }
-    
-    class AnySignUnitTests
+
+    class UnitTestsBase
     {
         private readonly _negative: boolean;
         constructor(negative: boolean)
@@ -30,24 +30,34 @@ module Tests
             this._negative = negative;
         }
 
-        public longInt(value: number | string): onCalc.LongInt
+        protected longInt(value: number | string): onCalc.LongInt
         {
             switch(typeof(value))
             {
                 case "string":
-                    return new onCalc.LongInt(this._negative ? "-" + value : value);
+                    return new onCalc.LongInt(this.str(<string>value));
                 case "number":
-                    return new onCalc.LongInt(this._negative ? -value : value);
+                    return new onCalc.LongInt(this.negative() ? -value : value);
                 default:
                     throw new Error("Initialize value type not supported");
             }
         }
 
-        public str(value: string): string
+        protected str(value: string): string
         {
-            return ((this._negative && value !== "0") ? ("-" + value) : value);
+            return ((this.negative() && value !== "0") ? ("-" + value) : value);
         }
- 
+
+        protected readonly negative = () => this._negative;
+    }
+
+    class AnySignUnitTests extends UnitTestsBase
+    {
+        constructor(negative: boolean)
+        {
+            super(negative);
+        }
+
         public numberToString(): void
         {
             let i = this.longInt(0);
@@ -57,15 +67,15 @@ module Tests
             i = this.longInt(100500);
             EXPECT_EQ(this.str("100500"), i.toString());
             EXPECT_EQ(1, i.size());
-            EXPECT_EQ(this._negative, i.negative());
+            EXPECT_EQ(this.negative(), i.negative());
             i = this.longInt(10000020000000);
             EXPECT_EQ(this.str("10000020000000"), i.toString());
             EXPECT_EQ(2, i.size());
-            EXPECT_EQ(this._negative, i.negative());
+            EXPECT_EQ(this.negative(), i.negative());
             i = this.longInt(9876543210123456);
             EXPECT_EQ(this.str("9876543210123456"), i.toString());
             EXPECT_EQ(3, i.size());
-            EXPECT_EQ(this._negative, i.negative());
+            EXPECT_EQ(this.negative(), i.negative());
         }
 
         public equalNumberAndStringConstruction(): void
@@ -93,6 +103,41 @@ module Tests
             EXPECT_EQ(x, y);
         } 
 
+        public equalOperatorTrue(): void
+        {
+            let eq = (x, y) =>
+            {
+                EXPECT_EQ(true, x.equal(x));
+                EXPECT_EQ(true, y.equal(y));
+                EXPECT_EQ(true, y.equal(x));
+                EXPECT_EQ(true, x.equal(y));
+            };
+            eq(this.longInt(0), this.longInt(0));
+            eq(this.longInt(10), this.longInt(10));
+            eq(this.longInt(100500), this.longInt(100500));
+            eq(this.longInt(99980000997766), this.longInt(99980000997766));
+            eq(this.longInt("9999999000000009999997733"), this.longInt("9999999000000009999997733"));
+            eq(this.longInt("9876543219876543210987654321123456789123456789123456789"), 
+               this.longInt("9876543219876543210987654321123456789123456789123456789"));
+        }
+
+        public equalOperatorFalseForSameSing(): void
+        {
+            let eq = (x, y) =>
+            {
+                EXPECT_EQ(false, y.equal(x));
+                EXPECT_EQ(false, x.equal(y));
+            };
+            eq(this.longInt(1), this.longInt(0));
+            eq(this.longInt(10), this.longInt(990010));
+            eq(this.longInt(100501), this.longInt(100500));
+            eq(this.longInt(99980000997766), this.longInt(99980000997767));
+            eq(this.longInt("9999999000000009999997732"), this.longInt("9999999000000009999997733"));
+            eq(this.longInt("9876543219876543210987654321123456789123456789123456789"), this.longInt(98));
+            eq(this.longInt("98765432198765432109876543211234567891234567891234567890000000001"), 
+               this.longInt("98765432198765432109876543211234567891234567891234567890000000000"));
+        }
+
         private add(): void
         {
             //99009900990099009900 + 998877665544332211 = 100008778655643342111
@@ -100,6 +145,55 @@ module Tests
             EXPECT_EQ("100008778655643342111", result.toString());
         }
     }
+
+    class signRelatedUnitTests extends UnitTestsBase
+    {
+        constructor()
+        {
+            super(false);
+        }
+
+
+        public lessPositiveAndPositive(): void
+        {
+            let x = this.longInt(0);
+            let y = this.longInt(10);
+            EXPECT_EQ(true, x.less(y));
+            EXPECT_EQ(false, y.less(x));
+        }
+
+        public equalOperatorFalse(): void
+        {
+            let eq = (x: string, y: string) =>
+            {
+                let lx = this.longInt(x);
+                let ly = this.longInt(y);
+                EXPECT_EQ(false, ly.equal(lx));
+                EXPECT_EQ(false, lx.equal(ly));
+                
+                lx = this.longInt("-" + x);
+                EXPECT_EQ(false, ly.equal(lx));
+                EXPECT_EQ(false, lx.equal(ly));
+                
+                ly = this.longInt("-" + y);
+                EXPECT_EQ(false, ly.equal(lx));
+                EXPECT_EQ(false, lx.equal(ly));
+
+                lx = this.longInt(x);
+                ly = this.longInt("-" + y);
+                EXPECT_EQ(false, ly.equal(lx));
+                EXPECT_EQ(false, lx.equal(ly));
+            };
+            eq("1", "2");
+            eq("100500", "100499");
+            eq("10000000000000500000000000000", "100500");
+            eq("9099990000500000000000000", "1059999999999988760");
+            eq("9876543219876543210987654321123456789123456789123456789", "98");
+            eq("98765432198765432109876543211234567891234567891234567890000000001", 
+               "98765432198765432109876543211234567891234567891234567890000000000");
+        }
+    }
+
     function RunAllTests(): void
     {
         try
@@ -107,10 +201,18 @@ module Tests
             let positive = new AnySignUnitTests(false);
             positive.numberToString();
             positive.equalNumberAndStringConstruction();
+            positive.equalOperatorTrue();
+            positive.equalOperatorFalseForSameSing();
 
             let negative = new AnySignUnitTests(true);
             negative.numberToString();
             negative.equalNumberAndStringConstruction();
+            negative.equalOperatorTrue();
+            negative.equalOperatorFalseForSameSing();
+
+            let sr = new signRelatedUnitTests();
+            sr.lessPositiveAndPositive();
+            sr.equalOperatorFalse();
 
             alert("ALL TESTS PASSED");
         }
